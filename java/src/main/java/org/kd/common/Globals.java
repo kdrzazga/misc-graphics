52fa04a99693440c6662987525692671d8470c63
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-12-05 17:45:32 +0100

    measuring time

--- java/src/main/java/org/kd/common/Globals.java
+++ java/src/main/java/org/kd/common/Globals.java
@@ -1,10 +1,12 @@
 package org.kd.common;
 
 public class Globals {
-    static public int SCREEN_WIDTH = 800;//1920;
-    static public int SCREEN_HEIGHT = 600;//1080;
+    static final public int SCREEN_WIDTH = 800;//1920;
+    static final public int SCREEN_HEIGHT = 600;//1080;
 
     static public C64Colors CURSOR_COLOR = C64Colors.LIGHT_BLUE;
     static public C64Colors BKG_COLOR = C64Colors.BLUE;
     static public long cursorY = Math.round(0.647 * Globals.SCREEN_HEIGHT) - 2;
+
+    static public long startTime = System.currentTimeMillis();
 }
