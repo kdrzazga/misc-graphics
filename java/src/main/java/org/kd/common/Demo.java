a99cdb1ba1ceddf473806430cc66b67ad950f5b6
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-12-08 11:16:02 +0100

    Improvements

--- java/src/main/java/org/kd/common/Demo.java
+++ java/src/main/java/org/kd/common/Demo.java
@@ -2,14 +2,26 @@ package org.kd.common;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class Demo {
+
+    protected static boolean fullscreen = true;
+
+    protected static void analyzeArguments(String[] args) {
+        Set<String> argsSet = new HashSet<>(Arrays.asList(args));
+        fullscreen = !(argsSet.contains("window") || argsSet.contains("--window") || argsSet.contains("nofullscreen"));
+    }
 
     public static LwjglApplicationConfiguration createConfig(String title) {
         var config = new LwjglApplicationConfiguration();
         config.title = title;
         config.width = Globals.SCREEN_WIDTH;
         config.height = Globals.SCREEN_HEIGHT;
-        config.fullscreen = true;
+        config.resizable = false;
+        config.fullscreen = fullscreen;
 
         return config;
     }
--- java/src/main/java/org/kd/winter/WinterDemo.java
+++ java/src/main/java/org/kd/winter/WinterDemo.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 public class WinterDemo extends org.kd.common.Demo {
 
     public static void main(String[] args) {
+        WinterDemo.analyzeArguments(args);
         var config = org.kd.common.Demo.createConfig("Winter Demo");
         new LwjglApplication(new WinterAnimationMgr(), config);
     }
