24be5c1963eca14ebc941349338e17066b5c103e
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-12-08 15:47:00 +0100

    Aniv demo - buildup

---
+++ java/src/main/java/org/kd/anniversaries/AnniversariesDemo.java
@@ -0,0 +1,14 @@
+package org.kd.anniversaries;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class AnniversariesDemo extends org.kd.common.Demo {
+
+    public static void main(String[] args) {
+        AnniversariesDemo.analyzeArguments(args);
+        AnniversariesDemo.fullscreen = false;
+        var config = org.kd.common.Demo.createConfig("Anniversaries Demo");
+        new LwjglApplication(new AnniversariesAnimationMgr(), config);
+
+    }
+}
