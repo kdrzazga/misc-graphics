ab7480d5ce926380526e3d9d71800b1eb4b10520
kdrzazga <krzysztof.drzazga@tutanota.com>
2026-01-06 20:27:30 +0100

    tombstone 1

---
+++ java/src/main/java/org/kd/goodjob/appendix/BannerTombstone.java
@@ -0,0 +1,58 @@
+package org.kd.goodjob.appendix;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BannerTombstone {
+    private final static List<String> l1 = Arrays.asList(" \n".repeat(25).split(""));
+
+    private final static List<String> l2 = Arrays.asList(
+
+     "qqqqqqqqqqqqqqqqqqqqqqqqqqqq+-----+qqqqqqqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqqqqqqq|qqqqq|qqqqqqqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqq+----+qqqqq+----+qqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqq|qqqqqqq+qqqqqqq|qqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqq+----+qqqqq+----+qqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqqqqqqq|qqqqq|qqqqqqqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqqqqqqq|qqqqq|qqqqqqqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqq___________|qqqqq|__________qqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqq/qqqqqqqqqqqqqqqqqqqqqqqqqqqq\\qqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqq/qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq\\qqqqqqqqqq"
+    ,"qqqqqqqqqqqqqq/qq****qq*****qqqqqq*qqq*qqqqqqqq\\qqqqqqqqq"
+    ,"qqqqqqqqqqqqq/qqq*qqqqqqq*qqqqqqqq*qqq*qqqqqqqqq\\qqqqqqqq"
+    ,"qqqqqqqqqqqq/qqqq*qqqqqqq*qqqqqqqq*qqq*qqqqqqqqqq\\qqqqqqq"
+    ,"qqqqqqqqqqq/qqqqq****qqqq*q*****qq*qqq*qq*****qqqq\\qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqq*qqqq*q*qqq*qq*qqq*qq*qqq*qqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqq*qqqq*q*****qq*qq*qqq*****qqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqq*qqqq*q*qqqqqq*q*qqqq*qqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqq****qqqq*q*****qq**qqqqq*****qqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqq********qqqqqqqqqq*qqqqqqqqqqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqq*qqqqqqqqqq*qqqqqqqqqqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqq*qq*****qqq*****qqq*****qqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqq*qq*qqq*qqq*qqq*qqq*qqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqq*qq*qqq*qqq*qqq*qqq*****qqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqq*qq*qqq*qqq*qqq*qqqqqqq*qqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqq*qq*****qqq*****qqq*****qqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqq*qqqqqqqqqqqqqqqqqqqqqqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqq*qqqqqq1955 - 2011qqqqqqqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqq*qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq|qqqqqq"
+    ,"qqqqqqqqqqq|qqq____qqq____qqq____qqq____qqq____qqq|qqqqqq"
+    ,"qqqqqqqqqqqq\\qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq\\qqqqq"
+    ,"qqqqqqqqqqqqq\\qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq\\qqqq"
+    ,"qqqqqqqqqqqqqq\\qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq\\qqq"
+    ,"qqqqqqqqqqqqqqq\\______________________________________\\qq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
+    ,"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"            
+    );
+
+    public static List<String> lines = new ArrayList<>(50);
+
+    static {
+        lines.addAll(l1);
+        lines.addAll(l2);
+    }
+}
